b863cc55e0b8fd2bd0098ecae6a43c8e6cf0fee8
Adia Dightman <makeda@Adias-MacBook-Pro.local>
2014-03-03 18:43:56 -0500

    created project added scaffold Image

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuteornot::Application.config.secret_key_base = 'f5abcd0b864dcd298945ade2308b2eca8485b66921078ddf9e0df3c665ec0f03dca5a8184aaddca4cf6a8a69f8bb671a9b882b4efe64d7e6a567aa95da332728'
